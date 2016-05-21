b4380fe8631c3e43f668a767018aa8765a70bd3d
Robpol86 <robpol86@gmail.com>
2016-05-21 12:45:23 -0700

    one more test

--- tests/test_build/test_build_border.py
+++ tests/test_build/test_build_border.py
@@ -70,6 +70,11 @@ def test_no_center(column_widths, left, right, expected):
     ([20, 0], 'Applications--------+'),
     ([0, 0, 20], 'Applications----------'),
     ([20, 0, 0], 'Applications--------++'),
+
+    ([10, 10], 'Applications---------'),
+    # ([11, 9], 'Applications---------'),
+    # ([12, 8], 'Applications+--------'),
+    # ([13, 7], 'Applications-+-------'),
 ])
 @pytest.mark.parametrize('left,right', [('', ''), ('<', '>')])
 def test_center(column_widths, left, right, expected):
